d5000d05b78721fc0cec9a081d81a2b438ad3dc4
frameur <francoisramos6121@gmail.com>
2020-12-05 19:35:25 +0100

    commit global first

---
+++ src/pages/Knowledges.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Knowledges = () => {
+    return (
+        <div>
+            <Navigation/>
+            connaissances
+        </div>
+    );
+};
+
+export default Knowledges;
