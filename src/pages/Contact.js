d324cfc3b8be4b55d52cf56e0db5ccf4aa78fdae
frameur <francoisramos6121@gmail.com>
2020-12-09 02:48:35 +0100

    creation project projectlist et contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
-        <div>
+    <div className='contact'>
             <Navigation/>
-            mon numéro
+        <div className='contactContent'>
+               <div className="header"></div>
+               <div className="contactBox">
+                   <h1>Contactez-moi</h1>
+                   <ul>
+                       <li >
+                           <i className="fas fa-map-marker-alt"></i>
+                           <span>Bellevigny</span>
+                       </li>
+                       <li>
+                           <i className="fas fa-mobile-alt"></i>
+                           <CopyToClipboard text="0642844284">
+                               <span className="clickInput" onClick={() => { alert('Téléphone copié !');}}>06 42 84 42 84</span>
+                           </CopyToClipboard>
+                       </li>
+                       <li>
+                           <i className="fas fa-envelope"></i>
+                           <CopyToClipboard text="francoisramos6121@gmail.com">
+                               <span className="clickInput" onClick={() => { alert('E-mail copié !');}}>francoisramos6121@gmail.com</span>
+                           </CopyToClipboard>
+                       </li>
+                   </ul>
+               </div>
+          <div className="socialNetwork">
+             <ul>
+                <a href="https://fr.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                    <h4>Linkedin</h4>
+                    <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                    <h4>Github</h4>
+                    <i className="fab fa-github"></i>
+                </a>
+                <a href="https://twitter.com/?lang=fr"target="_blank"rel="noopenernoreferrer">
+                    <h4>Twitter</h4>
+                    <i className="fab fa-twitter"></i>
+                </a>
+                <a href="https://codepen.io/" target="_blank" rel="noopener noreferrer">
+                    <h4>CodePen</h4>
+                    <i className="fab fa-codepen"></i>
+                </a>
+             </ul>   
+          </div>
         </div>
+    </div>
     );
 };
 
