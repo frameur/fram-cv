8e7ef16dbdd1b32b1f387ed71978a5fe04d19b8f
frameur <francoisramos6121@gmail.com>
2021-11-30 17:41:49 +0100

    ajout projet gastby pagination

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -62,12 +62,12 @@ export const portfolioData = [
   },
   {
     id: 6,
-    name: "",
-    languages: [],
+    name: "Mini projet Gatsby",
+    languages: ["javascript", "css", "reactjs"],
     languagesIcons: ["fab fa-js", "fab fa-css3-alt"],
-    source: "",
+    source: "https://gatsbypagination.netlify.app/",
     info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.",
-    picture: "./media/",
+    picture: "./media/gatsbypagination.png",
   },
   {
     id: 7,
