cec3fd5baf1742fba3d0ccf222974372fc1c32f3
frameur <francoisramos6121@gmail.com>
2022-10-20 17:27:28 +0200

    ajout 3

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -55,7 +55,7 @@ export const portfolioData = [
     name: 'Projet Breaking Bad',
     languages: ['javascript', 'css'],
     languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
-    source: 'https://frameur.github.io/projetbreaking/.github.com',
+    source: 'https://frameur.github.io/projetbreaking/',
     info: 'Petit projet, responsive breaking bad simple html, css, javascript.',
     picture: './media/page_breaking.png',
   },
