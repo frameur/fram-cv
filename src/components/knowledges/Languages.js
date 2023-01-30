f669051a2a1b413532d78e7496dea69f9301d594
frameur <francoisramos6121@gmail.com>
2023-01-30 19:43:01 +0100

    change valeur languages

--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -4,16 +4,16 @@ import ProgressBar from './ProgressBar'
 class Languages extends Component {
   state = {
     languages: [
-      { id: 1, value: 'Javascript', xp: 1.1 },
-      { id: 2, value: 'Css', xp: 1.2 },
+      { id: 1, value: 'Javascript', xp: 1.3 },
+      { id: 2, value: 'Css', xp: 1.4 },
       { id: 3, value: 'Php', xp: 0.2 },
       { id: 4, value: 'Python', xp: 0.1 },
     ],
     frameworks: [
-      { id: 1, value: 'Reactjs', xp: 0.8 },
-      { id: 2, value: 'Vuejs', xp: 0.3 },
+      { id: 1, value: 'Reactjs', xp: 1.3 },
+      { id: 2, value: 'Vuejs', xp: 0.5 },
       { id: 3, value: 'Bootstrap', xp: 0.3 },
-      { id: 4, value: 'material UI', xp: 0.2 },
+      { id: 4, value: 'material UI', xp: 0.5 },
     ],
   }
   render() {
