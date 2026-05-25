988b4f9cd7ce226825fd37068118ee2eb775ed35
frameur <francoisramos6121@gmail.com>
2026-05-25 21:20:53 +0200

    responsive

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,14 +1,9 @@
-import React, { useState, memo } from 'react'
+import React, { memo } from 'react'
 import { getLanguageIcons } from '../../data/portfolioData'
 
 const Project = memo(({ item }) => {
-  const [showInfo, setShowInfo] = useState(false);
-  const { name, languages, source, info, picture } = item;
+  const { name, languages, source, picture } = item;
   const languageIcons = getLanguageIcons(languages);
-
-  const toggleInfo = () => {
-    setShowInfo(!showInfo);
-  };
 
   return (
     <div className="project">
@@ -18,30 +13,10 @@ const Project = memo(({ item }) => {
         ))}
       </div>
       <h3>{name}</h3>
-      <img src={picture} alt={name} onClick={toggleInfo} />
-      {showInfo && (
-        <div className="showInfos">
-          <div className="infosContent">
-            <div className="head">
-              <h2>{name}</h2>
-              <div className="sourceCode">
-                <a
-                  href={source}
-                  rel="noopener noreferrer"
-                  className="button"
-                  target="_blank"
-                >
-                  Voir +
-                </a>
-              </div>
-            </div>
-            <p className="text">{info}</p>
-            <div className="button return" onClick={toggleInfo}>
-              Retour
-            </div>
-          </div>
-        </div>
-      )}
+      <img src={picture} alt={name} />
+      <a href={source} target="_blank" rel="noopener noreferrer" className="viewButton">
+        Voir le projet
+      </a>
     </div>
   );
 });
